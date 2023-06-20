f5759cd9ac2d3a9d16e676258342184f36d856fa
leh87 <ehlee@brainednet.com>
2023-06-20 20:31:56 +0900

    [1분코딩] 웹애니-API, scroll-box기준 progress 적용

--- @inflearn-ilbuni/web-animation-api/section6-scroll/waapi-scroll/01-basic/main.js
+++ @inflearn-ilbuni/web-animation-api/section6-scroll/waapi-scroll/01-basic/main.js
@@ -1,6 +1,7 @@
 import "./scroll-timeline.js";
 
 const progress = document.querySelector(".progress");
+const scrollBox = document.querySelector(".scroll-box");
 
 progress.animate(
   [
@@ -17,7 +18,10 @@ progress.animate(
         {
           // progress는 페이지의 처음부터 마지막까지에 영향을 받기 때문에 document.body
           // target = 해당 애니메이션이 받는 영역
-          target: document.body,
+          // target: document.body,
+
+          // target을 scroll-box로 하게 되면 브라우저 내 시작점에 왔을 때
+          target: scrollBox,
           edge: "start",
 
           // 1 = 0 지점으로 왔을 때 시작
@@ -25,7 +29,10 @@ progress.animate(
           threshold: 1,
         },
         {
-          target: document.body,
+          // target: document.body,
+
+          // target을 scroll-box로 하게 되면 브라우저 내 마지막점에 왔을 때
+          target: scrollBox,
           edge: "end",
           threshold: 1,
         },
